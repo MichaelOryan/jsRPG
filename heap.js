7346f5158db1d411d390f1a5496379233e7c56df
Michael O'Ryan <michael.oryan@gmail.com>
2016-11-14 20:11:08 +1100

    MinHeap
    TODO; test Heaps more to make sure no memory leaks.
    possibly reduce size of array when heap gets too small

--- heap.js
+++ heap.js
@@ -71,3 +71,77 @@ Algorithm.Heap.prototype.isEmpty = function()
 {
     return this.size == 0;
 };
+
+
+
+Algorithm.MinHeap = function()
+{
+    this.array = [];
+    this.size = 0;
+};
+
+
+Algorithm.MinHeap.prototype.siftUp = function(i)
+{
+    if(i <= 0)
+        return;
+    var parent = Math.floor(i / 2);
+    
+    if (this.array[parent] > this.array[i])
+    {
+        var tmp = this.array[parent];
+        this.array[parent] = this.array[i];
+        this.array[i] = tmp;
+        this.siftUp(parent);
+    }
+    
+};
+
+Algorithm.MinHeap.prototype.siftDown = function(i)
+{
+    if(i * 2 >= this.size)
+        return;
+    var child = i * 2;
+    if(child + 1 < this.size)
+    {
+        if(this.array[child] > this.array[child+1])
+            child++;
+    }
+    if(this.array[i] > this.array[child])
+    {
+        var tmp = this.array[i];
+        this.array[i] = this.array[child];
+        this.array[child] = tmp;
+        this.siftDown(child);
+    }
+};
+
+Algorithm.MinHeap.prototype.push = function(e)
+{
+    if(this.size == this.array.length)
+    {
+        this.array.push(e);
+    }
+    else
+    {
+        this.array[this.size] = e;
+    }
+    this.size++;
+    this.siftUp(this.size - 1);
+};
+
+Algorithm.MinHeap.prototype.pop = function()
+{
+    if(this.size == 0)
+        return;
+    var e = this.array[0];
+    this.size--;
+    this.array[0] = this.array[this.size];
+    this.siftDown(0);
+    return e;
+};
+
+Algorithm.MinHeap.prototype.isEmpty = function()
+{
+    return this.size == 0;
+};
